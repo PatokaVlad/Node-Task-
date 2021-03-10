d7f73c3a3fc0b055bae790f514f13139521a105e
Vlad Patoka <patrik.vv2002@gmail.com>
2021-03-11 00:41:03 +0200

    0.1

---
+++ main.js
@@ -0,0 +1,7 @@
+const { min, abs, summ } = require('math')
+
+let array = [3, 1, 4, 5, 9, 2]
+
+console.log(`Минимальное из - ${array}: ${min(array)}`)
+console.log(`Модуль из -228: ${abs(-228)}`)
+console.log(`Сума из - ${array}: ${summ(array)}`)
